fbc2d29b91c4cf2f2bca764fa3c43cd89de0caff
Leander Lindahl <leander@leanderlindahl.se>
2018-10-04 22:01:31 +0200

    Apply ESLint

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,46 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const RecipeDetail = (props) => {
-	if (!props.recipe) {
-		return (
-			<p 
-				style={props.style}
-				className={classNames('h3 p2 bg-white italic center', props.className)}
-			>
-				Please select a recipe to see the details. 
-			</p>
-		)
-	}
-	return (
-		<div 
-			style={props.style}
-			className={classNames('p2 bg-white', props.className)}
-		>
-			<h2 className="h2">{props.recipe.name}</h2>
-			<img src={props.recipe.image} className="fit" />
-			<div>
-				<span>{props.recipe.category}</span>
-				<span>{props.recipe.calories}</span>
-			</div>
-			<h3>Ingredients</h3>
-			<ul>
-				{props.recipe.ingredients.map(ingredient => (
-					<li key={ingredient}>
-						{ingredient}
-					</li>
-				))}
-			</ul>
-			<h3>Steps</h3>
-			<ol>
-				{props.recipe.steps.map(step => (
-					<li key={step}>
-						{step}
-					</li>
-				))}
-			</ol>
-		</div>
-	);
+  const { recipe, style, className } = props;
+  if (!recipe) {
+    return (
+      <p
+        style={style}
+        className={classNames('h3 p2 bg-white italic center', className)}
+      >
+        Please select a recipe to see the details.
+      </p>
+    );
+  }
+  return (
+    <div
+      style={style}
+      className={classNames('p2 bg-white', className)}
+    >
+      <h2 className="h2">{recipe.name}</h2>
+      <img
+        src={recipe.image}
+        alt={recipe.name}
+        className="fit"
+      />
+      <div>
+        <span>{recipe.category}</span>
+        <span>{recipe.calories}</span>
+      </div>
+      <h3>Ingredients</h3>
+      <ul>
+        {recipe.ingredients.map(ingredient => (
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+        ))}
+      </ul>
+      <h3>Steps</h3>
+      <ol>
+        {recipe.steps.map(step => (
+          <li key={step}>
+            {step}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+RecipeDetail.propTypes = {
+  recipe: PropTypes.object,
+  className: PropTypes.string,
+  style: PropTypes.object,
 };
 
 export default RecipeDetail;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,21 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RecipeList = (props) => (
-	<div style={props.style}>
-		<h2 className="h2">Recipes</h2>
-		<ul className="list-reset">
-			{props.recipes.map(recipe => (
-				<li 
-					key={recipe.id} 
-					className="py2 border-bottom border-bottom-dashed pointer"
-					onClick={() => props.onClick(recipe.id)}
-				>
-					<span>{ recipe.name }</span>
-					<span>{ recipe.category }</span>
-				</li>
-			))}
-		</ul>
-	</div>
+
+const RecipeList = ({ style, recipes, onClick }) => (
+  <div style={style}>
+    <h2 className="h2">Recipes</h2>
+    <ul className="list-reset">
+      {recipes.map(recipe => (
+        <li
+          key={recipe.id}
+          className="py2 border-bottom border-bottom-dashed pointer"
+          onClick={() => onClick(recipe.id)}
+        >
+          <span>{ recipe.name }</span>
+          <span>{ recipe.category }</span>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
+
+RecipeList.propTypes = {
+  recipes: PropTypes.array,
+  onClick: PropTypes.func,
+  style: PropTypes.object,
+};
 
 export default RecipeList;
