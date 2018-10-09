f08143946fa31d5d12b6e009c00c8547eadfa99f
Leander Lindahl <leander@leanderlindahl.se>
2018-10-09 10:32:07 +0200

    Add NotFound route fallback

--- webpack.config.js
+++ webpack.config.js
@@ -5,37 +5,40 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 require('dotenv').config();
 
 module.exports = {
-	context: path.join(__dirname, 'src'),
-	mode: "development",
-	entry: ['whatwg-fetch', './index.js'],
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: './bundle.js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.css$/,
-				loader: ['style-loader', 'css-loader', 'postcss-loader']
-			},
-			{
-				test: /\.(png|jpg)$/,
-				loader: 'file-loader'				
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './index.html',
-			inject: 'body'
-		}),
-		new webpack.DefinePlugin({
-			API_URL: JSON.stringify(process.env.API_URL)
-		})
-	]
+  context: path.join(__dirname, 'src'),
+  mode: 'development',
+  entry: ['whatwg-fetch', './index.js'],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: './bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: 'body',
+    }),
+    new webpack.DefinePlugin({
+      API_URL: JSON.stringify(process.env.API_URL),
+    }),
+  ],
 };
