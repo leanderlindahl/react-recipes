20388e6387d5fb2960d19acd52945829469ddcf4
Leander Lindahl <leander@leanderlindahl.se>
2018-10-11 09:41:47 +0200

    Mock static asset

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink } from 'react-router-dom';
+import Logo from '../static/images/logo.png';
 
 const HeaderLink = ({ children, ...props }) => (
   <NavLink
@@ -15,7 +16,11 @@ const HeaderLink = ({ children, ...props }) => (
 
 const Header = () => (
   <header className="flex align-center justify-between px4">
-    <h1 className="h1">🍽 My Recipes</h1>
+    <div>
+      <h1 className="h1 inline-block mr2">🍽 My Recipes</h1>
+      <span className="mr1">by</span>
+      <img src={Logo} alt="Logo" style={{ width: 30 }} />
+    </div>
     <nav>
       <HeaderLink to="/">Home</HeaderLink>
       <HeaderLink to="/favorites">Favorites</HeaderLink>
